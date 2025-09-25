6aa21b32ac3d27e024d7f870233a14a0bcf659ec
Leo-Pinto <leonardo.pinto@stu.ocsb.ca>
2025-09-25 20:42:32 +0000

    added random book generator wich is juist a reworked version of your coolness generator

--- Unit1/Final/js/script.js
+++ Unit1/Final/js/script.js
@@ -21,3 +21,19 @@ document.getElementById('clearButton').addEventListener('click', () => {
   document.getElementById('userInput').value = '';
   document.getElementById('userInput').focus();
 })
+
+
+
+function generateRandomName() {
+  const names = [
+    "3 body problem", "tress of the emerald sea", "mistborn", "the way of kings", "the will of the many"
+
+  ];
+
+  const randomIndex = Math.floor(Math.random() * names.length);
+  const randomName = names[randomIndex];
+
+  document.getElementById("generatedName").innerText = randomName;
+}
+
+
